f14bc04604a40d128560ea88652c9847a7114533
David LaMartina <lamartid@oregonstate.edu>
2019-04-25 09:58:07 -0700

    Minor changes before refactor

--- Program2/lamartid.buildrooms.c
+++ Program2/lamartid.buildrooms.c
@@ -11,6 +11,7 @@
 #define REQUIRED_ROOMS      7
 #define TOTAL_ROOMS         10
 #define MAX_CONNECTIONS     6
+#define DIR_NAME_FIRST      "lamartid.rooms."
 
 enum boolean { FALSE, TRUE };
 enum room_type { START_ROOM, MID_ROOM, END_ROOM };
@@ -19,7 +20,7 @@ struct room{
     int id;
     char* name;
     int numConnections;
-    struct room* connections[6];
+    struct room* connections[MAX_CONNECTIONS];
     enum room_type type;
 };
 
@@ -36,10 +37,11 @@ char* roomNames[10] = {
     "Healing_Well"
 };
 
+// Build name string for directory holding room files
 char* BuildDirName(){
-    char* first = "lamartid.rooms.";
+    char* first = DIR_NAME_FIRST;
     
-    const size_t max_pid_len = MAX_CONNECTIONS;   // Max 5 digits on os1 + null terminator
+    const size_t max_pid_len = 6;       // Max 5 digits on os1 + null terminator
     int pid = getpid();
     char* mypid = malloc(max_pid_len + 1);
     snprintf(mypid, max_pid_len, "%d", pid);
@@ -51,19 +53,20 @@ char* BuildDirName(){
     free(mypid);
     return dirName;
 }
-
+// Randomly select room names from hard-coded list
 void ChooseRoomNames(char* chosenRooms[], int numRooms){
     int i, choice;
     for (i = 0; i < numRooms; i++){
         do{
             choice = rand() % TOTAL_ROOMS;
         }while (roomNames[choice] == NULL);
+
         chosenRooms[i] = calloc(strlen(roomNames[choice]) + 1, sizeof(char));
         strcpy(chosenRooms[i], roomNames[choice]);
         roomNames[choice] = NULL;
     }
 }
-
+// Allocate memory to room structs and init variables
 void CreateRooms(struct room* rooms[], int numRooms, char* roomNames[]){
     int i, j;
     for (i = 0; i < numRooms; i++){
@@ -79,13 +82,13 @@ void CreateRooms(struct room* rooms[], int numRooms, char* roomNames[]){
         }else{
             rooms[i]->type = MID_ROOM;
         }
-
+        // Init all connections to null
         for (j = 0; j < MAX_CONNECTIONS; j++){
             rooms[i]->connections[j] = NULL;
         }
     }
 }
-
+// Determine whether graph has met min / max connection requirements
 enum boolean IsGraphFull(struct room* rooms[], int numRooms){
     int i;
     for (i = 0; i < numRooms; i++){
@@ -95,7 +98,7 @@ enum boolean IsGraphFull(struct room* rooms[], int numRooms){
     }
     return TRUE;
 }
-
+// Get random room ptr from set of room structs
 struct room* GetRandomRoom(struct room* rooms[], int numRooms){
     int i, choice;
     for (i = 0; i < numRooms; i++){
@@ -103,14 +106,14 @@ struct room* GetRandomRoom(struct room* rooms[], int numRooms){
     }
     return rooms[choice];
 }
-
+// Determine whether connection can be added (fewer than max already)
 enum boolean CanAddConnectionFrom(struct room* potentialRoom){
     if (potentialRoom->numConnections < 6){
         return TRUE;
     }
     return FALSE;
 }
-
+// Determine whether given connection already exists
 enum boolean ConnectionAlreadyExists(struct room* roomA, struct room* roomB){
     int i;
     for (i = 0; i < MAX_CONNECTIONS; i++){
@@ -120,15 +123,12 @@ enum boolean ConnectionAlreadyExists(struct room* roomA, struct room* roomB){
     }
     return FALSE;
 }
-
+// Connect rooms via pointers
 void ConnectRoom(struct room* roomA, struct room* roomB){
     roomA->connections[roomA->numConnections] = roomB;
     (roomA->numConnections)++;
-
-    // roomB->connections[roomB->numConnections] = roomA;
-    // (roomB->numConnections)++;
 }
-
+// Check room identity
 enum boolean IsSameRoom(struct room* roomA, struct room* roomB){
     if (roomA == roomB){
         return TRUE;
@@ -136,7 +136,7 @@ enum boolean IsSameRoom(struct room* roomA, struct room* roomB){
         return FALSE;
     }
 }
-
+// Randomly make double-sided connections between two compatible rooms in set
 void AddRandomConnection(struct room* rooms[], int numRooms){
     struct room* roomA;
     struct room* roomB;
@@ -156,14 +156,14 @@ void AddRandomConnection(struct room* rooms[], int numRooms){
     ConnectRoom(roomA, roomB);
     ConnectRoom(roomB, roomA);
 }
-
+// Build formatted string for room file
 char* BuildRoomString(struct room* newRoom){
     const int MAX_ROOM_DIGITS = 1;
     char* roomStr = calloc(1000, sizeof(char));
 
     char* nameIntro = "ROOM NAME: ";
     char* connIntro = "CONNECTION ";
-    char* typeIntro = "START_ROOM: ";
+    char* typeIntro = "ROOM TYPE: ";
     char* newLine   = "\n";
 
     strcpy(roomStr, nameIntro);
@@ -197,9 +197,9 @@ char* BuildRoomString(struct room* newRoom){
 
     return roomStr;
 }
-
+// Create all necessary files - 1 per room, formatted with BuildRoomString
 void CreateRoomFiles(struct room* rooms[], int numRooms, char* targetDir){
-    int i, roomFile, written;
+    int i, roomFile, written, bytesToWrite;
     char* slash = "/";
     char* roomStr;
     for (i = 0; i < numRooms; i++){
@@ -211,20 +211,23 @@ void CreateRoomFiles(struct room* rooms[], int numRooms, char* targetDir){
         strcat(roomPath, rooms[i]->name);
 
         roomFile = open(roomPath, O_WRONLY | O_CREAT, 0660);
-
         if (roomFile < 0){
-            printf("Could not open %s\n", roomPath);
+            printf("Could not create and open %s\n", roomPath);
+            exit(1);
         }
-
         roomStr = BuildRoomString(rooms[i]);
-        written = write(roomFile, roomStr, strlen(roomStr) * sizeof(char));
+        bytesToWrite = strlen(roomStr) * sizeof(char);
+        written = write(roomFile, roomStr, bytesToWrite);
+        if (written != bytesToWrite){
+            printf("Could not complete writing to %s\n", roomPath);
+            exit(1);
+        }
 
         free(roomPath);
         free(roomStr);
         close(roomFile);
     }
 }
-
 // Diagnostic print function
 void printRooms(struct room* rooms[], int numRooms){
     int i, j;
@@ -238,25 +241,25 @@ void printRooms(struct room* rooms[], int numRooms){
         printf("\n");
     }
 }
-
+// Driver
 int main(int argc, char* argv[]){
     srand(time(0));
-
+    // Create directory to be filled with room files
     char* dirName = BuildDirName();
     mkdir(dirName, S_IRWXU | S_IRWXG);
-
+    // Randomly pick names from list
     char* chosenRoomNames[REQUIRED_ROOMS];
     ChooseRoomNames(chosenRoomNames, REQUIRED_ROOMS);
-
+    // Create room structs
     struct room* rooms[REQUIRED_ROOMS];
     CreateRooms(rooms, REQUIRED_ROOMS, chosenRoomNames);
-
+    // Create connections until requirements met
     while (IsGraphFull(rooms, REQUIRED_ROOMS) == FALSE){
         AddRandomConnection(rooms, REQUIRED_ROOMS);
     }
-
+    // Create files with connected rooms
     CreateRoomFiles(rooms, REQUIRED_ROOMS, dirName);
-
+    // Free memory
     int i;
     for (i = 0; i < REQUIRED_ROOMS; i++){
         free(chosenRoomNames[i]);

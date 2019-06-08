5c54322f117861448754e8bbfe137151ddde2721
David LaMartina <lamartid@oregonstate.edu>
2019-06-08 08:16:28 -0700

    Generalized daemon. About to turn in

--- Program4/otp.c
+++ Program4/otp.c
@@ -90,3 +90,89 @@ void otpDecrypt( char* cipherText, char* key, char* plainText, char* charList )
         plainText[ i ] = charList[ plainVal ];
     }    
 }
+
+// Child process handling for encryption and decryption daemons
+void processConnection( int socketFD, enum OtpChoice EorD )
+{
+    int charsRead,          // Characters read from packet; must be checked against 0
+        charsSum,           // Sum of characters from multiple packets
+        charsWritten,       // Check to ensure all characters have been sent to client
+        msgSize;            // Size of total message -- need to get this first!
+
+    char correctClient;
+    if( EorD == ENC )
+        correctClient = OTP_ENC_ID;
+    else
+        correctClient = OTP_DEC_ID;
+
+    // Check identity of client (We know first message sent is identifying character)
+    // If wrong program, write error message to be interpreted for output on client side
+    char* clientChar = calloc( 1, sizeof(char) );               // Need char buffer to hold 1 char
+    charsRead = recv( socketFD, clientChar, sizeof(char), 0 );
+    if( charsRead < 0 ){
+        error( "SERVER: ERROR reading client ID from socket" );
+    }
+    if( clientChar[0] != correctClient ){
+        charsWritten = send( socketFD, OTP_BAD_CT, strlen( OTP_BAD_CT ), 0 );
+        if( charsWritten < 0 ){
+            error( "SERVER: ERROR writing bad connection error to socket" );
+        }
+        close( socketFD );  // Close down connection if client is unauthorized
+        exit( 0 );          // Server can exit successfully here; client's 'fault' for making bad connect
+    }
+
+    // Get the total size of the message to determine buffer length
+    // Credit: http://forums.codeguru.com/showthread.php?492913-Winsock-send()-Integers
+    charsRead  = recv( socketFD, (char* )&msgSize, sizeof(int), 0 );
+    if( charsRead < 0 ){
+        error( "SERVER: ERROR reading message size from socket" );
+    }
+ 
+    // Instantiate buffers for message and key based on received length
+    char* msgBuffer     = calloc( msgSize, sizeof(char) );
+    char* txtBuffer     = calloc( msgSize / 2, sizeof(char) );
+    char* keyBuffer     = calloc( msgSize / 2, sizeof(char) );
+    char* returnBuffer  = calloc( msgSize / 2, sizeof(char) );
+
+    // Read in plain text and key as one large message - necessary because breaking up into
+    // segments removes our ability to differentiate until we have the whole thing.
+    // Must provide the address of the current spot in the array on each iteration! Otherwise
+    // multiple iterations will override the priors, never exceeding a CHUNK_SIZE segment in length.
+    charsSum = 0;
+    do{
+        charsRead = recv( socketFD, &(msgBuffer[ charsSum ]), CHUNK_SIZE, 0 );
+        charsSum += charsRead;
+        if( charsRead < 0 ){
+            error( "SERVER: ERROR reading message from socket" );
+        }
+    }while( charsSum < msgSize );
+
+    // Split plain text and key - we can assume they're each exactly half the message buffer,
+    // and we know they were sent in order 1. message 2. key
+    memcpy( txtBuffer, msgBuffer, (msgSize / 2) * sizeof(char) );
+    memcpy( keyBuffer, &(msgBuffer[ msgSize / 2 ]), (msgSize / 2) * sizeof(char) );
+
+    // Encrypt or decrypt message with key, storing result in return buffer
+    char* charList = CHAR_LIST;                                     // Out char list stored in otp.h as macro
+    if( EorD == ENC )
+        otpEncrypt( txtBuffer, keyBuffer, returnBuffer, charList );
+    else
+        otpDecrypt( txtBuffer, keyBuffer, returnBuffer, charList );
+
+    // Send return buffer back to client
+    charsWritten = send( socketFD, returnBuffer, msgSize / 2, 0 );  // Send back w/ known size
+    if( charsWritten < 0 ){
+        error( "SERVER: ERROR writing return buffer to socket" );
+    }
+
+    // Clean up
+    free( clientChar    );
+    free( msgBuffer     );
+    free( txtBuffer     );
+    free( keyBuffer     );
+    free( returnBuffer  );
+
+    // Exit successfully after all messages properly received and sent
+    close( socketFD );
+    exit( 0 );
+}
--- Program4/otp.h
+++ Program4/otp.h
@@ -10,18 +10,27 @@
 #ifndef OTP_H
 #define OTP_H
 
-#define CHAR_LIST           "ABCDEFGHIJKLMNOPQRSTUVWXYZ "  // List of valid characters for otp program
+#define CHAR_LIST           "ABCDEFGHIJKLMNOPQRSTUVWXYZ "   // List of valid characters for otp program
+#define CHUNK_SIZE          1000                            // Size of byte chunks received
 
 // Identifiers to be sent before messages to ensure correct programs connecting to daemons
 #define OTP_ENC_ID          'e'
 #define OTP_DEC_ID          'd'
 
 // Bad connection flag -- sent from server to access-restricted client
-#define OTP_BAD_CT          "b"
-#define OTP_BAD_CT_RESPONSE 'b'
+#define OTP_BAD_CT          "b"     // String for sending
+#define OTP_BAD_CT_RESPONSE 'b'     // Character for receiving
 
+// Toggle for encryption / decryption in child process handling
+enum OtpChoice { ENC, DEC };
+
+// Encryption code
 void otpEncrypt( char*, char*, char*, char* );
 
+// Decryption code
 void otpDecrypt( char*, char*, char*, char* );
+
+// Child process handling for encryption and decryption daemons
+void processConnection( int, enum OtpChoice );
 
 #endif
